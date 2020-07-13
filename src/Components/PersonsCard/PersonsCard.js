3e23fc1fe035c93681d1c95ce0860742475f65ef
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-07-13 20:16:15 +0430

    change avatar size

--- src/Components/PersonsCard/PersonsCard.js
+++ src/Components/PersonsCard/PersonsCard.js
@@ -14,7 +14,7 @@ export default class personsCard extends React.Component {
                 <div className="card">
                     <div className="imgdiv">
                         
-                        <img className="contactImg" src={"https://ui-avatars.com/api/?name=" + Contacts.name+"+"+Contacts.lastname+"&background="+randomColor+"&color=fff"} />
+                        <img className="contactImg" src={"https://ui-avatars.com/api/?name=" + Contacts.name+"+"+Contacts.lastname+"&background="+randomColor+"&color=fff"+"&size=50"} />
                     </div>
                     <div className="informationdiv">
                         <p className="nameText">{Contacts.name + " " + Contacts.lastname}</p>
