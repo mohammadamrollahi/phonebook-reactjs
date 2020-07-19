7e7b7fd1cc1a8d7f21e5a51d8bab8eeaea3b266d
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2020-07-19 21:46:57 +0430

    Updates

---
+++ precache-manifest.0dea2950f1395681e74584a94dea18ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ec940e336bb0bc104d173bdc0e12f3",
+    "url": "/phonebook-reactjs/index.html"
+  },
+  {
+    "revision": "a8575833d669d528fb99",
+    "url": "/phonebook-reactjs/static/css/main.5677c838.chunk.css"
+  },
+  {
+    "revision": "16932f4dbe63c6743881",
+    "url": "/phonebook-reactjs/static/js/2.790235ad.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/phonebook-reactjs/static/js/2.790235ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8575833d669d528fb99",
+    "url": "/phonebook-reactjs/static/js/main.2ea0434a.chunk.js"
+  },
+  {
+    "revision": "a20e75d9d0bb1f5dfacf",
+    "url": "/phonebook-reactjs/static/js/runtime-main.989152ba.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phonebook-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
